8e16062de4ed643c278e5d2f90cafab56ef4a98c
AlexandrAnatoliev <per-1986@list.ru>
2022-12-01 22:31:38 +0300

    init

---
+++ main.py
@@ -0,0 +1,45 @@
+# pythonProject3
+
+# БОТ, ВЕДУЩИЙ TELEGRAM-КАНАЛ С АНЕКДОТАМИ
+# По нажатию кнопки бот присылает случайный анекдот в личку
+
+import telebot
+import random
+from telebot import types
+from config import token
+
+# Загружаем список анекдотов из файла
+# если текстовый файл находится не в каталоге программы, то пишем полный путь к нему
+# "C:/Users/Александр/OneDrive/Рабочий стол/python/FreelanceTask2/freelanceTask3/firstText.txt" (использ.:'/'!)
+f = open('fun.txt', 'r', encoding='UTF-8')
+funs = f.read().split('\n')
+f.close()
+
+# Создаем бота
+bot = telebot.TeleBot(token)
+
+
+# Команда start
+@bot.message_handler(commands=["start"])
+def start(m, res=False):
+    # Добавляем кнопку
+    markup = types.ReplyKeyboardMarkup(resize_keyboard=True)
+    item1 = types.KeyboardButton("Анекдот")
+    markup.add(item1)
+    bot.send_message(m.chat.id,
+                     'Нажми: \nАнекдот для получения интересного анекдота ',
+                     reply_markup=markup)
+
+
+# Получение сообщений от юзера
+@bot.message_handler(content_types=["text"])
+def handle_text(message):
+    # Если юзер прислал 1, выдаем ему случайный анекдот
+    if message.text.strip() == 'Анекдот':
+        answer = random.choice(funs)
+    # Отсылаем юзеру сообщение в его чат
+    bot.send_message(message.chat.id, answer)
+
+
+# Запускаем бота
+bot.polling(none_stop=True, interval=0)
